84af40bf2cf3b4520a81f2bce2a6b6ba6c7229b8
maikonFoltz <maikonjfoltz@gmail.com>
2020-11-21 09:45:55 -0300

    não funcionou

---
+++ index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Clima</title>
+    <style>
+        #res{
+            width: 150px;
+            height: 100px;
+            font-size: 40px;
+            text-align: center;
+        }
+    </style>
+</head>
+<body>
+    <input type="text" id="cidade">
+    <button>Pesquisar</button>
+    <div id="res">
+        0
+    </div>
+
+   <script src="jquery.min.js"></script>
+   <script src="script.js"></script> 
+</body>
+</html>
---
+++ script.js
@@ -0,0 +1,9 @@
+$(function(){
+
+    $('button').bind('click', function(){
+        
+        var cidade = $('#cidade').val();
+        var now = new Date();
+        
+    })
+})
